15beb72ca1a81fc0d272ca7292d2c09a80625486
SEAVLING <116346546+SEAVLING@users.noreply.github.com>
2022-12-14 08:16:29 +0700

    Add files via upload

--- recursion.js
+++ recursion.js
@@ -1,7 +1,7 @@
 const isEven = num => {
     if (num == 0) return true;
     if (num == 1) return false;
-    if (num < 0) return isEven((num-2)* -1)
+    if (num < 0) return isEven(num * -1)
     return isEven(num -2)
 }
 console.log(isEven(50));
